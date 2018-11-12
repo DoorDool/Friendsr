df6e11fb106028cf18b2e682aaa5056b9314c32e
DoorDool <dorindavandendool@hotmail.nl>
2018-11-12 11:50:43 +0100

    Initial Friend

---
+++ app/src/main/java/com/example/dorin/friendsr/Friend.java
@@ -0,0 +1,19 @@
+package com.example.dorin.friendsr;
+
+import java.io.Serializable;
+
+public class Friend implements Serializable {
+
+    private String name, bio;
+    private int drawableId;
+    private float rating;
+
+    public Friend(String name, String bio, int drawableId) {
+        this.name = name;
+        this.bio = bio;
+        this.drawableId = drawableId;
+    }
+
+
+
+}
